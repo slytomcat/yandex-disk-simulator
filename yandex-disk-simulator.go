88a0aa4ccdc2bedc94c977bcc33ac015677bc75d
Sly_tom_cat <slytomcat@mail.ru>
2019-01-05 00:50:33 +0300

    code review

--- yandex-disk-simulator.go
+++ yandex-disk-simulator.go
@@ -24,6 +24,24 @@ type Event struct {
 const (
 	daemonLogFile = "/tmp/yandexdisksymulator.log"
 	socketPath    = "/tmp/yandexdisksymulator.socket"
+	helpMsg       = `Usage:
+	yandex-disk-similator <cmd>
+Commands:
+	start	starts the daemon and begin starting events simulation
+	stop	stops the daemon
+	status	get the daemon status
+	sync	begin the synchronisation events simulation
+	help    show this help message 
+	setup 	prepares the simulation environment. It creates the cofiguration and 
+		token files in Sim_ConfDir and the syncronization directory in Sim_SyncDir.
+		Environment variables Sim_ConfDir and Sim_SyncDir should be set in advance, 
+		otherways the default paths will be used.
+		Setup process doesn't requere any input in the terminal.
+Simulator commands:
+	daemon	start as a daemon (Don't use it !!!)
+Environment variables:
+	Sim_SyncDir	can be used to set synchronized directory path (default: ~/Yandex.Disk)
+	Sim_ConfDir	can be used to set configuration directory path (default: ~/.config/yandex-disk)`
 )
 
 var (
@@ -36,7 +54,7 @@ var (
 	startTime = 1000
 
 	// start sequence
-	startSequence = []Event{
+	startSequence = &[]Event{
 		Event{" ",
 			time.Duration(1600) * time.Millisecond,
 			""},
@@ -55,44 +73,33 @@ var (
 	}
 
 	// sync sequence
-	syncSequence = []Event{
+	syncSequence = &[]Event{
 		Event{"Synchronization core status: index\nPath to Yandex.Disk directory: '/home/stc/Yandex.Disk'\n\tTotal: 43.50 GB\n\tUsed: 2.89 GB\n\tAvailable: 40.61 GB\n\tMax file size: 50 GB\n\tTrash size: 0 B\n\nLast synchronized items:\n\tfile: 'File.ods'\n\tfile: 'downloads/file.deb'\n\tfile: 'downloads/setup'\n\tfile: 'download'\n\tfile: 'down'\n\tfile: 'do'\n\tfile: 'd'\n\tfile: 'o'\n\tfile: 'w'\n\tfile: 'n'\n\n",
 			time.Duration(900) * time.Millisecond,
-			"Sync simulation started"},
+			"Synchronization simulation started"},
 		Event{"Sync progress: 65.34 MB/ 139.38 MB (46 %)\nSynchronization core status: index\nPath to Yandex.Disk directory: '/home/stc/Yandex.Disk'\n\tTotal: 43.50 GB\n\tUsed: 2.89 GB\n\tAvailable: 40.61 GB\n\tMax file size: 50 GB\n\tTrash size: 0 B\n\nLast synchronized items:\n\tfile: 'File.ods'\n\tfile: 'downloads/file.deb'\n\tfile: 'downloads/setup'\n\tfile: 'download'\n\tfile: 'down'\n\tfile: 'do'\n\tfile: 'd'\n\tfile: 'o'\n\tfile: 'w'\n\tfile: 'n'\n\n",
 			time.Duration(100) * time.Millisecond,
-			"Sync simulation 1"},
+			"Synchronization simulation 1"},
 		Event{"Sync progress: 65.34 MB/ 139.38 MB (46 %)\nSynchronization core status: busy\nPath to Yandex.Disk directory: '/home/stc/Yandex.Disk'\n\tTotal: 43.50 GB\n\tUsed: 2.89 GB\n\tAvailable: 40.61 GB\n\tMax file size: 50 GB\n\tTrash size: 0 B\n\nLast synchronized items:\n\tfile: 'File.ods'\n\tfile: 'downloads/file.deb'\n\tfile: 'downloads/setup'\n\tfile: 'download'\n\tfile: 'down'\n\tfile: 'do'\n\tfile: 'd'\n\tfile: 'o'\n\tfile: 'w'\n\tfile: 'n'\n\n",
 			time.Duration(1500) * time.Millisecond,
-			"Sync simulation 2"},
-		Event{"Sync progress: 139.38 MB/ 139.38 MB (100 %)\nSynchronization core status: index\nPath to Yandex.Disk directory: '/home/stc/Yandex.Disk'\n\tTotal: 43.50 GB\n\tUsed: 2.89 GB\n\tAvailable: 40.61 GB\n\tMax file size: 50 GB\n\tTrash size: 0 B\n\nLast synchronized items:\n\tfile: 'File.ods'\n\tfile: 'downloads/file.deb'\n\tfile: 'downloads/setup'\n\tfile: 'download'\n\tfile: 'down'\n\tfile: 'do'\n\tfile: 'd'\n\tfile: 'o'\n\tfile: 'w'\n\tfile: 'n'\n\n",
+			"Synchronization simulation 2"},
+		Event{"Sync progress: 139.38 MB/ 139.38 MB (100 %)\nSynchronization core status: index\nPath to Yandex.Disk directory: '/home/stc/Yandex.Disk'\n\tTotal: 43.50 GB\n\tUsed: 2.89 GB\n\tAvailable: 40.61 GB\n\tMax file size: 50 GB\n\tTrash size: 0 B\n\nLast synchronized items:\n\tfile: 'NewFile'\n\tfile: 'File.ods'\n\tfile: 'downloads/file.deb'\n\tfile: 'downloads/setup'\n\tfile: 'download'\n\tfile: 'down'\n\tfile: 'do'\n\tfile: 'd'\n\tfile: 'o'\n\tfile: 'w'\n\n",
 			time.Duration(500) * time.Millisecond,
-			"Sync simulation 3"},
+			"Synchronization simulation 3"},
 	}
 )
 
-func simulateSync(l io.Writer) {
+func simulate(name string, seq *[]Event, l io.Writer) {
 	symLock.Lock()
-	for _, e := range syncSequence {
+	for _, e := range *seq {
 		setMsg(e.msg)
-		l.Write([]byte(e.log + "\n"))
+		if e.log != "" {
+			l.Write([]byte(e.log + "\n"))
+		}
 		time.Sleep(e.duration)
 	}
 	setMsg(msgIdle)
-	l.Write([]byte("Sync finished\n"))
-	symLock.Unlock()
-}
-
-func simulateStart(l io.Writer) {
-	// start sequence
-	symLock.Lock()
-	for _, e := range startSequence {
-		setMsg(e.msg)
-		l.Write([]byte(e.log + "\n"))
-		time.Sleep(e.duration)
-	}
-	setMsg(msgIdle)
-	l.Write([]byte("Start simulation finished\n"))
+	l.Write([]byte(name + " simulation finished\n"))
 	symLock.Unlock()
 }
 
@@ -111,129 +118,72 @@ func notExists(path string) bool {
 	return false
 }
 
-func main() {
+func initLog() *os.File {
 	dlog, err := os.OpenFile(daemonLogFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755)
 	if err != nil {
 		log.Fatal(daemonLogFile, "opening error:", err)
 	}
-	defer dlog.Close()
 	log.SetOutput(dlog)
+	return dlog
+}
+
+func main() {
 	if len(os.Args) == 1 {
 		fmt.Println("Error: command hasn't been specified. Use the --help command to access help\nor setup to launch the setup wizard.")
 		os.Exit(1)
-	} else {
-		cmd := os.Args[1]
-		if len(cmd) > 16 {
-			cmd = cmd[0:16]
+	}
+	loggingFile := initLog()
+	defer loggingFile.Close()
+	cmd := os.Args[1]
+	if len(cmd) > 8 {
+		cmd = cmd[0:8]
+	}
+	switch cmd {
+	case "daemon":
+		daemon()
+	case "start":
+		checkCfg()
+		if notExists(socketPath) {
+			daemonize()
+		} else {
+			fmt.Println("Daemon is already running.")
 		}
-		switch cmd {
-		case "daemon":
-			daemon()
-		case "start":
-			checkCfg()
-			if notExists(socketPath) {
-				daemonize()
-				fmt.Print("Starting daemon process...")
-				time.Sleep(time.Duration(startTime) * time.Millisecond)
-				fmt.Println("Done")
-			} else {
-				fmt.Println("Daemon is already running.")
-			}
-		case "--help", "help":
-			fmt.Println(`Usage:
-	yandex-disk-similator <cmd>
-Commands:
-	start	starts the daemon and begin starting events simulation
-	stop	stops the daemon
-	status	get the daemon status
-	sync	begin the synchronisation events simulation
-	help    show this help message 
-	setup 	prepares the simulation environment. It creates the cofiguration and 
-		token files in Sim_ConfDir and the syncronization directory in Sim_SyncDir.
-		Environment variables Sim_ConfDir and Sim_SyncDir should be set in advance, 
-		otherways the default paths will be used.
-		Setup process doesn't requere any input in the terminal.
-Simulator commands:
-	daemon	start as a daemon (Don't use it !!!)
-Environment variables:
-	Sim_SyncDir	can be used to set synchronized directory path (default: ~/Yandex.Disk)
-	Sim_ConfDir	can be used to set configuration directory path (default: ~/.config/yandex-disk)`)
-		case "status", "stop", "sync":
-			// if notExists(os.ExpandEnv(syncPath)) {
-			// 	fmt.Println("Error: Indicated directory does not exist")
-			// 	os.Exit(1)
-			// } else {
-			socketSend(cmd)
-			// }
-		case "setup":
-			cfgPath = os.Getenv("Sim_ConfDir")
-			if cfgPath == "" {
-				cfgPath = os.ExpandEnv("$HOME/Yandex.Disk")
-			}
-			syncPath = os.Getenv("Sim_SyncDir")
-			if syncPath == "" {
-				syncPath = os.ExpandEnv("$HOME/.config/yandex-disk")
-			}
-			err := os.MkdirAll(cfgPath, 0777)
-			if err != nil {
-				log.Fatal("Config path creation error")
-			}
-			auth := filepath.Join(cfgPath, "passwd")
-			if notExists(auth) {
-				tfile, err := os.OpenFile(auth, os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0666)
-				if err != nil {
-					log.Fatal("yandex-disk token file creation error:", err)
-				}
-				defer tfile.Close()
-				_, err = tfile.Write([]byte("token")) // yandex-disk-simulator doesn't require the real token
-				if err != nil {
-					log.Fatal("yandex-disk token file creation error:", err)
-				}
-			}
-			cfg := filepath.Join(cfgPath, "config.cfg")
-			cfile, err := os.OpenFile(cfg, os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0666)
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer cfile.Close()
-			_, err = cfile.Write([]byte("proxy=\"no\"\nauth=\"" + auth + "\"\ndir=\"" + syncPath + "\"\n\n"))
-			if err != nil {
-				log.Fatal("Can't create config file: ", err)
-			}
-			err = os.MkdirAll(syncPath, 0777)
-			if err != nil {
-				log.Fatal("synchronization Dir creation error:", err)
-			}
-		default:
-			fmt.Println("Error: unknown command: '" + cmd + "'")
-			os.Exit(1)
-		}
+	case "status", "stop", "sync":
+		// only listed commands will be passed to daemon
+		socketIneract(cmd)
+	case "setup":
+		setup()
+	case "-h", "--help", "help":
+		fmt.Println(helpMsg)
+	default:
+		fmt.Println("Error: unknown command: '" + cmd + "'")
+		os.Exit(1)
 	}
 }
 
 func daemonize() {
+	fmt.Print("Starting daemon process...")
 	// get executable name
 	_, exe := filepath.Split(os.Args[0])
 	// execute it with daemon command
 	cmd := exec.Command(exe, "daemon")
-	//cmd.Stdout = os.Stderr
+	//cmd.Stdout = os.Stdout
 	//cmd.Stderr = os.Stderr
 	err := cmd.Start()
 	if err != nil {
 		log.Fatal(err)
 	}
+	time.Sleep(time.Duration(startTime) * time.Millisecond)
+	fmt.Println("Done")
 }
 
 func daemon() {
-	dlog, err := os.OpenFile("/tmp/yandexdiskmock.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755)
-	if err != nil {
-		log.Fatal("/tmp/yandexdiskmock.log opening error:", err)
-	}
-	defer dlog.Close()
-	log.SetOutput(dlog)
+	loggingFile := initLog()
+	defer loggingFile.Close()
 	log.Println("Daemon started")
+	defer log.Println("Daemon stopped")
 	// disconnect from terminal
-	_, err = syscall.Setsid()
+	_, err := syscall.Setsid()
 	if err != nil {
 		log.Fatal("syscall.Setsid() error:", err)
 	}
@@ -254,7 +204,7 @@ func daemon() {
 		log.Fatal(logFilePath+" opening error:", err)
 	}
 	defer func() {
-		logfile.Write([]byte("exit\n"))
+		logfile.Write([]byte("exit/n"))
 		logfile.Close()
 	}()
 	// open socket as server
@@ -262,15 +212,15 @@ func daemon() {
 	if err != nil {
 		log.Fatal("Listen error: ", err)
 	}
-	// defer closing of socket and log file
+	// defer closing of socket
 	defer ln.Close()
 
-	// start simulation of start
-	go simulateStart(logfile)
+	// begin start simulation
+	go simulate("Start", startSequence, logfile)
 
-	buf := make([]byte, 16)
+	buf := make([]byte, 8)
 	for {
-		// read next command from socket
+		// accept and read next command from socket
 		fd, err := ln.Accept()
 		if err != nil {
 			return
@@ -283,55 +233,52 @@ func daemon() {
 		log.Println("Received:", cmd)
 		// react on command ...
 		switch cmd {
-		case "stop":
-			return
-		case "status":
+		case "status": // replay to socket by current message
 			msgLock.Lock()
 			fd.Write([]byte(message))
 			msgLock.Unlock()
-		case "sync":
-			go simulateSync(logfile)
+		case "sync": // begin the synchronization simulation
+			go simulate("Synchronization", syncSequence, logfile)
 			fd.Write([]byte(" "))
-		default:
-			fd.Write([]byte("Error: unknown command: '" + cmd + "'"))
+		case "stop": // stop the daemon
+			return
+			//default: there is no other options (should be) possible
 		}
 	}
 }
 
-func socketSend(cmd string) {
+func socketIneract(cmd string) {
 	if notExists(socketPath) {
-		// output error to stdot and exit with nonzero error code
+		// output error to stdout and exit with nonzero error code
 		fmt.Println("Error: daemon not started")
 		os.Exit(1)
-	} else {
-		// open socket as client
-		c, err := net.Dial("unix", socketPath)
-		if err != nil {
-			log.Fatal("Socket dial error", err)
-		}
-		defer c.Close()
-		// send cmd to socket
-		_, err = c.Write([]byte(cmd))
-		if err != nil {
-			log.Fatal("Socket write error", err)
-		}
-		buf := make([]byte, 512)
-		// wait for replay
-		n, err := c.Read(buf[:])
-		if err != nil {
-			if err == io.EOF {
-				fmt.Println("Daemon stopped.")
-				return
-			}
-			log.Fatal("Socket read error", err)
-		}
-		// output replay to stdout
-		m := string(buf[0:n])
-		if m != " " {
-			fmt.Println(m)
-		}
 	}
-	return
+	// open socket as client
+	c, err := net.Dial("unix", socketPath)
+	if err != nil {
+		log.Fatal("Socket dial error", err)
+	}
+	defer c.Close()
+	// send cmd to socket
+	_, err = c.Write([]byte(cmd))
+	if err != nil {
+		log.Fatal("Socket write error", err)
+	}
+	// read reply
+	buf := make([]byte, 512)
+	n, err := c.Read(buf[:])
+	if err != nil {
+		if err == io.EOF { // closed socket mean that daemon was stopped
+			fmt.Println("Daemon stopped.")
+			return
+		}
+		log.Fatal("Socket read error", err)
+	}
+	// output non-empty reply to stdout
+	m := string(buf[0:n])
+	if m != " " {
+		fmt.Println(m)
+	}
 }
 
 func checkCfg() {
@@ -377,5 +324,46 @@ func checkCfg() {
 	if notExists(auth) {
 		fmt.Println("Error: file with OAuth token hasn't been found.\nUse 'token' command to authenticate and create this file")
 		os.Exit(1)
+	}
+}
+
+func setup() {
+	cfgPath = os.Getenv("Sim_ConfDir")
+	if cfgPath == "" {
+		cfgPath = os.ExpandEnv("$HOME/Yandex.Disk")
+	}
+	syncPath = os.Getenv("Sim_SyncDir")
+	if syncPath == "" {
+		syncPath = os.ExpandEnv("$HOME/.config/yandex-disk")
+	}
+	err := os.MkdirAll(cfgPath, 0777)
+	if err != nil {
+		log.Fatal("Config path creation error")
+	}
+	auth := filepath.Join(cfgPath, "passwd")
+	if notExists(auth) {
+		tfile, err := os.OpenFile(auth, os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0666)
+		if err != nil {
+			log.Fatal("yandex-disk token file creation error:", err)
+		}
+		defer tfile.Close()
+		_, err = tfile.Write([]byte("token")) // yandex-disk-simulator doesn't require the real token
+		if err != nil {
+			log.Fatal("yandex-disk token file write error:", err)
+		}
+	}
+	cfg := filepath.Join(cfgPath, "config.cfg")
+	cfile, err := os.OpenFile(cfg, os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cfile.Close()
+	_, err = cfile.Write([]byte("proxy=\"no\"\nauth=\"" + auth + "\"\ndir=\"" + syncPath + "\"\n\n"))
+	if err != nil {
+		log.Fatal("Can't create config file: ", err)
+	}
+	err = os.MkdirAll(syncPath, 0777)
+	if err != nil {
+		log.Fatal("synchronization Dir creation error:", err)
 	}
 }
