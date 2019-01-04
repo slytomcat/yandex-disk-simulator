92d858f1a35c881234c973c495074f9425515723
Sly_tom_cat <slytomcat@mail.ru>
2019-01-04 16:36:38 +0300

    more tests

--- yandex-disk-simulator_test.go
+++ yandex-disk-simulator_test.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/exec"
 	"testing"
+	"time"
 )
 
 var (
@@ -79,6 +80,76 @@ func Example25SecondStart() {
 	cmd.Run()
 	// Output:
 	// Daemon is already running.
+}
+
+func Example40StatusAfterStart() {
+	cmd := exec.Command("yandex-disk-simulator", "status")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stdout
+	cmd.Run()
+	// Output:
+	//
+}
+
+func Example50StatusAfter600MSeconds() {
+	time.Sleep(600 * time.Millisecond)
+	cmd := exec.Command("yandex-disk-simulator", "status")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stdout
+	cmd.Run()
+	// Output:
+	// Synchronization core status: paused
+	// 	Path to Yandex.Disk directory: '/home/stc/Yandex.Disk'
+	// 			The quota has not been received yet.
+	// 	Last synchronized items:
+	// 			file: 'File.ods'
+	// 			file: 'downloads/file.deb'
+	// 			file: 'downloads/setup'
+	// 			file: 'download'
+	// 			file: 'down'
+	// 			file: 'do'
+	// 			file: 'd'
+	// 			file: 'o'
+	// 			file: 'w'
+	// 			file: 'n'
+}
+
+func Example60StatusAfter6Seconds() {
+	time.Sleep(6000 * time.Millisecond)
+	cmd := exec.Command("yandex-disk-simulator", "status")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stdout
+	cmd.Run()
+	// Output:
+	// Synchronization core status: idle
+	// 	Path to Yandex.Disk directory: '/home/stc/Yandex.Disk'
+	// 			Total: 43.50 GB
+	// 			Used: 2.89 GB
+	// 			Available: 40.61 GB
+	// 		Max file size: 50 GB
+	// 			Trash size: 0 B
+	//
+	// 	Last synchronized items:
+	// 			file: 'File.ods'
+	// 			file: 'downloads/file.deb'
+	// 			file: 'downloads/setup'
+	// 			file: 'download'
+	// 			file: 'down'
+	// 			file: 'do'
+	// 			file: 'd'
+	// 			file: 'o'
+	// 			file: 'w'
+	// 			file: 'n'
+	//
+}
+
+func Example70Sync() {
+	cmd := exec.Command("yandex-disk-simulator", "sync")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stdout
+	cmd.Run()
+	// Output:
+	//
 }
 
 func Example90Stop() {
