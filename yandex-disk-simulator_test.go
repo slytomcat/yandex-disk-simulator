ab872659a1b7eca558d7aa59e1bf863e0cae74c2
Sly_tom_cat <slytomcat@mail.ru>
2019-01-04 12:22:22 +0300

    more tests

--- yandex-disk-simulator_test.go
+++ yandex-disk-simulator_test.go
@@ -1,24 +1,82 @@
 package main
 
 import (
+	"flag"
 	"os"
 	"os/exec"
+	"testing"
 )
 
-func init() {
+var (
+	SyncDirPath    = "$HOME/TeSt_Yandex.Disk_TeSt"
+	ConfigFilePath = "$HOME/.config/TeSt_Yandex.Disk_TeSt"
+)
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+
 	exec.Command("go", "build", "yandex-disk-simulator.go").Run()
 	cwd, _ := os.Getwd()
 	path := os.Getenv("PATH")
 	os.Setenv("PATH", cwd+":"+path)
+	SyncDirPath = os.ExpandEnv(SyncDirPath)
+	os.Setenv("Sim_SyncDir", SyncDirPath)
+	ConfigFilePath = os.ExpandEnv(ConfigFilePath)
+	os.Setenv("Sim_ConfDir", ConfigFilePath)
+
+	// Run tests
+	e := m.Run()
+
+	// Clearance
+	exec.Command("yandex-disk-simulator", "stop").Run()
+	os.RemoveAll(ConfigFilePath)
+	os.RemoveAll(SyncDirPath)
+
+	os.Exit(e)
 }
 
-func Example1() {
-	cmd := exec.Command("go", "run", "yandex-disk-simulator.go")
+func Example01StartWithoutCommand() {
+	cmd := exec.Command("yandex-disk-simulator")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stdout
 	cmd.Run()
 	// Output:
 	// Error: command hasn't been specified. Use the --help command to access help
 	// or setup to launch the setup wizard.
-	// exit status 1
+}
+
+func Example02StartUnconfigured() {
+	cmd := exec.Command("yandex-disk-simulator", "start")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stdout
+	cmd.Run()
+	// Output:
+	// Error: option 'dir' is missing --
+}
+
+func Example03StartSetup() {
+	cmd := exec.Command("yandex-disk-simulator", "setup")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stdout
+	cmd.Run()
+	// Output:
+	//
+}
+
+func Example04StartSuccess() {
+	cmd := exec.Command("yandex-disk-simulator", "start")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stdout
+	cmd.Run()
+	// Output:
+	// Starting daemon process...Done
+}
+
+func Example05SecondStart() {
+	cmd := exec.Command("yandex-disk-simulator", "start")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stdout
+	cmd.Run()
+	// Output:
+	// Daemon is already running.
 }
